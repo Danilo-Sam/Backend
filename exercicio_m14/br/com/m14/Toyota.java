13dc6170bc70076fb76d1599629fc572a1377dd5
DaniloSam <danilobrown17@gmail.com>
2024-04-17 17:45:32 -0300

    exercicio m_14

---
+++ exercicio_m14/br/com/m14/Toyota.java
@@ -0,0 +1,18 @@
+public class Toyota implements Carro {
+
+	@Override
+	public String getNome() {
+		return "Toyota";
+
+	}
+
+	@Override
+	public String getModelo() {
+		return "Corolla";
+	}
+
+	@Override
+	public String toString() {
+		return "Fabricante = " + getNome() + ", Modelo = " + getModelo();
+	}
+}
