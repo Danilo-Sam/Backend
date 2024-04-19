56599b106c46301cda1fc5c49ff0ebfeb513d9ac
DaniloSam <danilobrown17@gmail.com>
2024-04-19 18:54:54 -0300

    corrigindo erros

--- exercicio_m17/br/com/m17/Carro.java
+++ exercicio_m17/br/com/m17/Carro.java
@@ -1,6 +1,26 @@
 package br.com.m17;
 
-public interface Carro {
-    String getNome();
-    String getModelo();
+abstract class Carro {
+	
+	private String marca;
+	private String modelo;
+	
+	public Carro(String marca, String modelo) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	
+	public abstract void exibirDetalhes();
+	
 }
